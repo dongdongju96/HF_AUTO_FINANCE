c7417e5ffa6b830174f3fe6d9729224c37b019c3
dongdongju96 <leedongju15@gmail.com>
2025-01-10 17:28:01 -0500

    Modify cash price and ah elem id

--- RBC/input_data.py
+++ RBC/input_data.py
@@ -1746,7 +1746,8 @@ class DealerTrackAutomation:
                         self.enter_trade_in_field("BalanceOwedTo", "Trade-In Lender")
 
     def enter_trade_in_field(self, field_id, field_name):
-        input_field = self.driver.find_element(By.ID, f"ctl21_ctl24_ctl00_txt{field_id}")
+        # ctl21_ctl23_ctl00_txtYear
+        input_field = self.driver.find_element(By.ID, f"ctl21_ctl23_ctl00_txt{field_id}")
         input_field.clear()
         input_field.send_keys(self.data["fields"].get(field_name, ""))
         entered_value = input_field.get_attribute("value")
@@ -1760,6 +1761,7 @@ class DealerTrackAutomation:
         entered_value = input_field.get_attribute("value")
         print(f"Entered {field_name}: {entered_value}")
     def enter_cash_down_payment(self):
+        # ctl21_ctl26_ctl00_txtCashDownPayment
         if "Cash Down Payment" in self.data["fields"]:
             self.enter_field("CashDownPayment", "Cash Down Payment")
 
@@ -1773,7 +1775,7 @@ class DealerTrackAutomation:
     def enter_gap_insurance_amount(self):
         wait = WebDriverWait(self.driver, 20)
         if "Gap Insurance Amount" in self.data["fields"]:
-            gap_insurance_input = wait.until(EC.element_to_be_clickable((By.ID, "ctl21_ctl29_ctl00_txtAHInsurance")))
+            gap_insurance_input = wait.until(EC.element_to_be_clickable((By.ID, "ctl21_ctl27_ctl00_txtAHInsurance")))
             gap_insurance_input.click()
             gap_insurance_input.send_keys(str(self.data["fields"].get("Gap Insurance Amount", "")))
             entered_value = gap_insurance_input.get_attribute("value")
