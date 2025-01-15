adca4e09203ac73444dbc9b7f134a71d1fefe614
dongdongju96 <leedongju15@gmail.com>
2025-01-15 13:23:17 -0500

    Check test250115

--- NorthLake/input_data.py
+++ NorthLake/input_data.py
@@ -2184,9 +2184,10 @@ class DealerTrackAutomation:
             self.select_amortization()
 
             self.select_payment_frequency()
-
-            self.select_interest_rate()
-
+            try:
+                self.select_interest_rate()
+            except:
+                pass
             # NBC에는 없는 옵션
             # self.enter_scene_card()
 
